47a6e2e8c4178e412f8f9c91df2ba4cda6d6346c
Zanetta Norris <znorris@zipcodes-MacBook-Pro.local>
2020-02-06 17:54:39 -0500

    Added method oneToNumber. All tests passed.

--- SumOfInput.java
+++ SumOfInput.java
@@ -9,16 +9,21 @@ public class SumOfInput
     // instance variables - replace the example below with your own
     private Integer sum;
 
+
     /**
      * Constructor for objects of class SumOfInput
      */
     public SumOfInput()
     {
         // initialise instance variables
-        sum = 0;
+
     }
-    
+
     public Integer oneToNumber(Integer numberToSum){
+       sum = 0;
+            for (int x =1; x<=numberToSum; x++){
+              sum += x;
+            }
 
         return sum;
     }
